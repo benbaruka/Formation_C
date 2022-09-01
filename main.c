5fdc77df5d32a083cb656cfe512e2982432b604f
= <=>
2022-08-31 17:21:16 -0700

    add commentaire

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-
+//Programme qui calculer le produit de deux nombres nbr1 et nbr2 entrEs par l'utilisateur
 int main(void)
 {
+    //declaration de ces deux variable nbr1 et nbr2
   int nbr1, nbr2;
 
-
+//on entre le deux nombres a l'ecran
   printf("Entrez le premier facteur: "); scanf("%d",&nbr1);
 
   printf("Entrez le deuxieme facteur: "); scanf("%d",&nbr2);
+  // calcul du produit et affichage de resultat a l'ecran
 
   printf("le produit de %d et %d = %d" , nbr1 , nbr2 , nbr1*nbr2);
 
