c7c63da4f7d69010eed6e785cb4a557f792bafd6
= <=>
2022-08-31 17:12:35 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int nbr1, nbr2;
+
+
+  printf("Entrez le premier facteur: "); scanf("%d",&nbr1);
+
+  printf("Entrez le deuxieme facteur: "); scanf("%d",&nbr2);
+
+  printf("le produit de %d et %d = %d" , nbr1 , nbr2 , nbr1*nbr2);
+
+
+
+
+}
