b34dad32100dcb83790decce5068aa66009b0d6c
= <=>
2022-08-31 18:13:37 -0700

    add commentair exercice 3

--- exercice3.c
+++ exercice3.c
@@ -2,10 +2,12 @@
 //Exercice 3 Ecrire un programme qui détermine si un entier saisi est pair ou impair.
 void main()
 {
+    //declaration de la variable 
     int nombre_saisi = 0;
-    
-    printf("Entrez un nombre:",nombre_saisi);
+    //l'utilisateur entre un nombre a l'ecran
+    printf("Entrez un nombre:");
     scanf("%d", &nombre_saisi);
+    //teste sur un nombre est pair ou impair
     if(nombre_saisi%2){
         printf("le nombre %d est impair",nombre_saisi);
     }
